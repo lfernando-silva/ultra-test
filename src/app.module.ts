9fb1e37c5f921656cf18fd291f8fdb5b10f38122
Luiz Fernando <lfernando-silva@hotmail.com>
2021-12-14 20:04:07 -0300

    feat: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
